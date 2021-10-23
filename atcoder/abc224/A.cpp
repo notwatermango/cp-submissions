3e3bc468ce12a87443cd0194328f1557d4f4f6d1
notwatermango <notwatermango@gmail.com>
2021-10-23 19:01:00 +0700

    Add solution for problem `A - Tires` Link: https://atcoder.jp/contests/abc224/tasks/abc224_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc224/submissions/26754255

---
+++ atcoder/abc224/A.cpp
@@ -0,0 +1,35 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+// #define int int64_t
+#define MOD 1000000007
+#define LLINF 4202000013666420666
+#define INF 1000177013
+
+void solve();
+
+signed main(){
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  int tc = 1;
+  // cin >> tc;
+  for(int i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solve();
+  }
+  
+  return 0;
+}
+
+void solve(){
+  string st;
+  cin >> st;
+  int n = st.length();
+  if(st[n-2] == 'e' && st[n-1] == 'r'){
+    cout << "er\n";
+  } else {
+    cout << "ist\n";
+  }
+}
