ca8f1512f591d1def9f8ea7fbdd0d6f9646e7ae2
notwatermango <notwatermango@gmail.com>
2021-10-24 18:52:00 +0700

    Add solution for problem `D - Vupsen, Pupsen and 0` Link: https://codeforces.com/contest/1582/problem/D Tags: constructive algorithms, math, *1600 Ref: https://codeforces.com/contest/1582/submission/132909341

--- codeforces/1582/D.cpp
+++ codeforces/1582/D.cpp
@@ -1,82 +1,72 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <iomanip>
 using namespace std;
 
-typedef int64_t i64;
-typedef int32_t i32;
-
-i32 water = !not!iscntrl(isupper(isgraph(iscntrl(ispunct(isalpha(EOF))))));
-#define mango(x)
-#define fastmango cin.tie(nullptr)->sync_with_stdio(water)
-#define precisemango(x) cout << fixed << setprecision(x)
-#ifdef notwatermango
-#undef mango
-#undef notwatermango
-#define mango(x) { water = 999; auto x_ = x; water = not water; cerr<<ends<<"\033[38;5;206m"<<__LINE__<<"\t: "<<#x<<" = "<<x_<<"\e[39m"<<'\n'; }
-#endif
-#define notwatermango void solv()
-i64 powllm(i64 a, i64 b, i64 m) { a %= m; i64 res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; }
-i64 powll(i64 a, i64 b) { i64 res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
-template<typename Water, typename ...Typename>
-Water& operator<<(Water&water,pair<Typename...>&p){return water<<'{'<<get<0>(p)<<", "<<get<1>(p)<<'}';}
-template<typename Water, typename Fire>
-Water& operator<<(Water&water,vector<vector<Fire>>v){water<<"\n{\n";for(auto i:v){water<<"\t[ ";for(auto j: i)water\
-// <<left<<setw(8) \笑
-<<j<<' ';water<<"]\n";};return water<<"}";}
-template<typename Water>
-Water& operator<<(Water&water,string&v){for(auto i: v)water<<i;return water;}
-template<typename Water, typename Fire>
-Water& operator<<(Water&water,Fire&v){water<<'[';for(auto i: v)water<<i<<", ";return water<<']';}
-template<typename Mango>
-vector<Mango> tov(Mango x[], i32 xs) { return vector<Mango> (x, x + xs); }
-
+// #define int int64_t
+#define MOD 1000000007
+#define LLINF 4202000013666420666
 #define INF 1000177013
-#define LLINF 4202025013666420666
-#define dbg(x) tov(x, sizeof x / sizeof x[0]);
-#define all(x) (x).begin(), (x).end()
-#define sz(x) (int)(x).size()
-#define mp make_pair
-#define int i64
-#define MOD                                          1000000007
-#define YSS                                          112345
 
-notwatermango;
+void solve();
 
-signed main() {
-  fastmango;
-  i32 tc = 1;
+signed main(){
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  int tc = 1;
   cin >> tc;
-  for (i32 i = 0; i < tc; ++i) {
+  for(int i = 0; i < tc; ++i) {
     // cout << "Case #" << i+1 << ": ";
-    solv();
+    solve();
   }
-  return water;
+  
+  return 0;
 }
 
-void solv() {
-  int n; cin >> n;
+void solve(){
+  int n;
+  cin >> n;
   vector<int> arr(n);
-  for (int i = 0; i < n; ++i) cin >> arr[i];
-  if (n % 2) {
-    for (int i = 0; i < n-3; i += 2) {
-      cout << -arr[i+1] << ' ' << arr[i] << ' ';
-    }
-    if (arr[n-3] + arr[n-2] != 0) {
-      mango('q');
-      cout << -arr.back() << ' ' << -arr.back() << ' ';
-      cout << (arr[n-3] + arr[n-2]) << '\n';
-    } else if (arr[n-3] + arr.back() != 0) {
-      mango('w');
-      cout << -arr[n-2] << ' ';
-      cout << (arr[n-3] + arr.back()) << ' ';
-      cout << -arr[n-2] << '\n';
-    } else {
-      mango('e');
-      cout << (arr[n-2] + arr[n-1]) << ' ';
-      cout << -arr[n-3] << ' ' << -arr[n-3] << '\n';
-    }
-  } else {
-    for (int i = 0; i < n; i += 2) {
-      cout << -arr[i+1] << ' ' << arr[i] << ' ';
-    } cout << '\n';
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
   }
+  vector<int> res(n);
+  for (int i = 0; i < n; i+=2)
+  {
+    res[i] = arr[i+1]*-1;
+    res[i+1] = arr[i];
+    int gc = gcd(res[i] , res[i+1]);
+    res[i]/=gc;
+    res[i+1]/=gc;
+  }
+  if(n%2 == 1){
+    for (int i = 0; i < n-1; i++)
+    {
+    
+    if(abs(res[i] - arr[n-1]) <= 10000 && abs(res[i] - arr[n-1]) != 0){
+      
+        res[n-1] = arr[i];
+        res[i] += arr[n-1]*-1;
+      break;
+    } else if(abs(res[i] + arr[n-1]) <= 10000 && abs(res[i] + arr[n-1]) != 0){
+        res[n-1] = arr[i]*-1;
+        res[i] += arr[n-1];
+        break;
+    }
+    }
+  }
+  // int sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    cout << res[i] << ' ';
+  }
+  cout << '\n';
+  
+  
+
+   
 }
