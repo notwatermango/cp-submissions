81ba86b6b8b38fb0921026cf06a5ab24e6418ebe
notwatermango <notwatermango@gmail.com>
2021-10-29 18:55:00 +0700

    Add solution for problem `A - Bear and Prime 100` Link: https://codeforces.com/contest/679/problem/A Tags: constructive algorithms, interactive, math, *1400 Ref: https://codeforces.com/contest/679/submission/133441744

---
+++ codeforces/679/A.cpp
@@ -0,0 +1,66 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = not true;
+#define mango(x)
+#define fastmango ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#ifdef notwatermango
+#undef notwatermango
+#undef mango
+#define mango(x) {++water; auto x_=x; --water; cerr<<string(water, '\t')<<"\033[38;5;206m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<'\n';}
+#endif
+#define notwatermango void solv();
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& water,  const Cont& v){ water<<"["; for(auto& x:v) water<<x<<", "; return water<<"]"; }
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& water,  const pair<Ts...>& p){ return water<<"{"<<p.first<<", "<<p.second<<"}"; }
+
+#define MOD 1000000007
+#define LLINF 4202025013666420666
+#define INF 1000177013
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+// #define int i64
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  // cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return 0;
+}
+
+void solv(){
+  int ct = 0;
+  for(auto e: {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47}){
+    cout << e << endl;
+    string s;
+    cin >> s;
+    if(s == "yes"){
+      ct++;
+      if(e*e < 101) cout << e*e << endl;
+      else continue;
+      cin >> s;
+      if(s == "yes"){
+        ct++;
+      }
+    }
+    if(ct > 1) break;
+
+  }
+  if(ct>1){
+    cout << "composite" << endl; 
+  } else cout << "prime" << endl;
+}
