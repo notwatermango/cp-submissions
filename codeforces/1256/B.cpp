a4b9afdadf0d267a195eb08dcf30bac4b94b46e1
notwatermango <notwatermango@gmail.com>
2021-11-08 11:44:00 +0700

    Add solution for problem `B - Minimize the Permutation` Link: https://codeforces.com/contest/1256/problem/B Tags: greedy, *1400 Ref: https://codeforces.com/contest/1256/submission/134627600

---
+++ codeforces/1256/B.cpp
@@ -0,0 +1,76 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = not true;
+#define mango(x)
+#define fastmango ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#ifdef notwatermango
+#undef notwatermango
+#undef mango
+#define mango(x) {++water; auto x_=x; --water; cerr<<string(water, '\t')<<"\033[38;5;206m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<'\n';}
+#endif
+#define notwatermango void solv();
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& water,  const Cont& v){ water<<"["; for(auto& x:v) water<<x<<", "; return water<<"]"; }
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& water,  const pair<Ts...>& p){ return water<<"{"<<p.first<<", "<<p.second<<"}"; }
+
+#define MOD 1000000007
+#define LLINF 4202025013666420666
+#define INF 1000177013
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+// #define int i64
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return 0;
+}
+
+void solv(){
+  int n; cin >> n;
+  int arr[n];
+  bool ct[n];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+    ct[i] = false;
+  }
+  for (int i = 1; i < n+1; i++)
+  {
+    for (int j = 1; j < n; j++)
+    {
+      if(arr[j] == i && !ct[j] && arr[j] < arr[j-1]){
+        ct[j] = true;
+        int tmp = arr[j];
+        arr[j] = arr[j-1];
+        arr[j-1] = tmp;
+        j--;
+        j--;
+        j = max(j, 0);
+      }
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << ' ';
+  }
+  cout << '\n';
+  
+  
+}
