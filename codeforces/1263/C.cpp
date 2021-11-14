b08c165fb63991ac1194159156f9a47861a18c63
notwatermango <notwatermango@gmail.com>
2021-11-14 12:03:00 +0700

    Add solution for problem `C - Everyone is a Winner!` Link: https://codeforces.com/contest/1263/problem/C Tags: binary search, math, meet-in-the-middle, number theory, *1400 Ref: https://codeforces.com/contest/1263/submission/135339423

---
+++ codeforces/1263/C.cpp
@@ -0,0 +1,80 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = not true;
+#define mango(x)
+#define fastmango ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#ifdef notwatermango
+#undef notwatermango
+#undef mango
+#define mango(x) {++water; auto x_=x; --water; cerr<<string(water, '\t')<<"\033[38;5;206m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<'\n';}
+#endif
+#define notwatermango void solv();
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& water,  const Cont& v){ water<<"["; for(auto& x:v) water<<x<<", "; return water<<"]"; }
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& water,  const pair<Ts...>& p){ return water<<"{"<<p.first<<", "<<p.second<<"}"; }
+
+#define MOD 1000000007
+#define LLINF 4202025013666420666
+#define INF 1000177013
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define int i64
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return 0;
+}
+
+void solv(){
+  int n; cin >> n;
+  vector<int> v;
+  for (int i = 1; i * i<=  n; i++)
+  {
+    v.push_back(i);
+  }
+  for (int i = sz(v)-1; i > -1; i--)
+  {
+    if(n/v[i] != v.back()){
+
+      v.push_back(n/v[i]);
+    }
+  }
+
+
+  cout << sz(v)+1<<'\n' << 0;
+  for(auto e:v){
+    cout << ' ' << e;
+  }  
+  cout << '\n';
+
+  
+
+  // mango(v);
+  
+
+}
+
+/*
+11
+6
+0 1 2 3 5 11
+*/
