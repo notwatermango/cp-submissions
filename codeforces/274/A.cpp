740fe73c75e80da69412560e77a416d124e09f33
notwatermango <notwatermango@gmail.com>
2021-11-15 21:35:00 +0700

    Add solution for problem `A - k-Multiple Free Set` Link: https://codeforces.com/contest/274/problem/A Tags: binary search, greedy, sortings, *1500 Ref: https://codeforces.com/contest/274/submission/135753507

---
+++ codeforces/274/A.cpp
@@ -0,0 +1,70 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = not true;
+#define mango(x)
+#define fastmango ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#ifdef notwatermango
+#undef notwatermango
+#undef mango
+#define mango(x) {++water; auto x_=x; --water; cerr<<string(water, '\t')<<"\033[38;5;206m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<'\n';}
+#endif
+#define notwatermango void solv();
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& water,  const Cont& v){ water<<"["; for(auto& x:v) water<<x<<", "; return water<<"]"; }
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& water,  const pair<Ts...>& p){ return water<<"{"<<p.first<<", "<<p.second<<"}"; }
+
+#define MOD 1000000007
+#define LLINF 4202025013666420666
+#define INF 1000177013
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define int i64
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  // cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return 0;
+}
+
+void solv(){
+  int n, k;
+  cin >> n >> k;
+  int x;
+  set<int> S;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> x;
+    S.insert(x);
+  }
+  if(k == 1){
+    cout << n << '\n';
+    return;
+  }
+  int ct = 0;
+  for(auto e: S){
+    ct++;
+    S.erase(e*k); // err when k == 1;
+  }
+  
+  cout << ct << '\n';
+  
+
+}
