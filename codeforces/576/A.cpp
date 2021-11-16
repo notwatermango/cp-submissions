38aded513c4cc56718abc8f4c8100eeeb72aafa9
notwatermango <notwatermango@gmail.com>
2021-11-16 14:09:00 +0700

    Add solution for problem `A - Vasya and Petya's Game` Link: https://codeforces.com/contest/576/problem/A Tags: math, number theory, *1500 Ref: https://codeforces.com/contest/576/submission/135835237

---
+++ codeforces/576/A.cpp
@@ -0,0 +1,91 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = not true;
+#define mango(x)
+#define fastmango ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#ifdef notwatermango
+#undef notwatermango
+#undef mango
+#define mango(x) {++water; auto x_=x; --water; cerr<<string(water, '\t')<<"\033[38;5;206m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<'\n';}
+#endif
+#define notwatermango void solv();
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& water,  const Cont& v){ water<<"["; for(auto& x:v) water<<x<<", "; return water<<"]"; }
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& water,  const pair<Ts...>& p){ return water<<"{"<<p.first<<", "<<p.second<<"}"; }
+
+#define MOD 1000000007
+#define LLINF 4202025013666420666
+#define INF 1000177013
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+// #define int i64
+
+const int N = 10000;
+vector<int> lp(N+1);
+vector<int> pr;
+
+void sieve(){
+  for (int i=2; i <= N; ++i) {
+    if (lp[i] == 0) {
+      lp[i] = i;
+      pr.push_back(i);
+    }
+    for (int j=0; j < sz(pr) && pr[j] <= lp[i] && i*pr[j] <= N; ++j) {
+      lp[i * pr[j]] = pr[j];
+    }
+  }
+}
+
+void sad_sieve(){
+  for (int i = 2; i <= N; i++)
+  {
+    if(lp[i] != i) continue;
+    for (int j = i; j < sz(pr); j*=i)
+    {
+      lp[j] = -1; 
+    }
+  }
+}
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  // cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return 0;
+}
+
+void solv(){
+  sieve();
+  sad_sieve();
+  int n; cin >> n;
+  if(n == 1) {cout << "0\n"; return;}
+  int ct = 0;
+  for (int i = 2; i <= n; i++)
+  {
+    if(lp[i] == -1) ct++;
+  }
+  cout << ct << '\n';
+  for (int i = 2; i <= n; i++)
+  {
+    if(lp[i] == -1) cout << i << ' ';
+  }
+  cout << '\n';
+  
+  
+}
