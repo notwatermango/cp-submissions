de5a0077b524203c65fc6ca83d8d2db66e36f97a
notwatermango <notwatermango@gmail.com>
2021-11-17 20:51:00 +0700

    Add solution for problem `A - Tile Painting` Link: https://codeforces.com/contest/1242/problem/A Tags: constructive algorithms, math, number theory, *1500 Ref: https://codeforces.com/contest/1242/submission/135976935

---
+++ codeforces/1242/A.cpp
@@ -0,0 +1,72 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = not true;
+#define mango(x)
+#define fastmango ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#ifdef notwatermango
+#undef notwatermango
+#undef mango
+#define mango(x) {++water; auto x_=x; --water; cerr<<string(water, '\t')<<"\033[38;5;206m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<'\n';}
+#endif
+#define notwatermango void solv();
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& water,  const Cont& v){ water<<"["; for(auto& x:v) water<<x<<", "; return water<<"]"; }
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& water,  const pair<Ts...>& p){ return water<<"{"<<p.first<<", "<<p.second<<"}"; }
+
+#define MOD 1000000007
+#define LLINF 4202025013666420666
+#define INF 1000177013
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define int i64
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  // cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return 0;
+}
+
+void solv(){
+  int n; cin >> n;
+  set<int> S;
+  int i = 2;
+  int qq;
+  for(; i <= min(n, 1000005ll); ++i){
+    qq = i;
+    while(n % i == 0){
+      S.insert(i);
+      n/=i;
+    }
+  }
+  i--;
+  mango(S); 
+  int szs = sz(S);
+  int res = n;
+  if(szs == 1 && *S.begin() != n){
+    res = i;
+    if(i == 1000005ll){
+      res = 1;
+    }
+  }
+  if(szs > 1){
+    res = 1;
+  }
+  cout << res << '\n';
+}
