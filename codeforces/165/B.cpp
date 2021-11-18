2d91287dc549bd2dc83740c4ff8f01f716e3b382
notwatermango <notwatermango@gmail.com>
2021-11-18 11:04:00 +0700

    Add solution for problem `B - Burning Midnight Oil` Link: https://codeforces.com/contest/165/problem/B Tags: binary search, implementation, *1500 Ref: https://codeforces.com/contest/165/submission/136028449

---
+++ codeforces/165/B.cpp
@@ -0,0 +1,68 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = not true;
+#define mango(x)
+#define fastmango ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#ifdef notwatermango
+#undef notwatermango
+#undef mango
+#define mango(x) {++water; auto x_=x; --water; cerr<<string(water, '\t')<<"\033[38;5;206m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<'\n';}
+#endif
+#define notwatermango void solv();
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& water,  const Cont& v){ water<<"["; for(auto& x:v) water<<x<<", "; return water<<"]"; }
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& water,  const pair<Ts...>& p){ return water<<"{"<<p.first<<", "<<p.second<<"}"; }
+
+#define MOD 1000000007
+#define LLINF 4202025013666420666
+#define INF 1000177013
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define int i64
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  // cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return 0;
+}
+
+int n, k;
+
+bool vasya(int v){
+  int res = 0;
+  int qq = 1;
+  while(1){
+    res += v/qq;
+    if(v/qq == 0) break;
+    qq *= k;
+  }
+  if(res < n) return false;
+  return true;
+}
+
+void solv(){
+  cin >> n >> k;
+  //n or more
+  int res = 0;
+  for(int i = INF; i > 0; i/=2){
+    while(!vasya(i + res)) res+=i;
+  }
+  cout << res + 1 << '\n';
+}
