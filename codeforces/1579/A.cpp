a3299fc279b3786dd4104b413a6e53fe42253b4f
notwatermango <notwatermango@gmail.com>
2021-11-24 18:08:00 +0700

    Add solution for problem `A - Casimir's String Solitaire` Link: https://codeforces.com/contest/1579/problem/A Tags: math, strings, *800 Ref: https://codeforces.com/contest/1579/submission/136731950

---
+++ codeforces/1579/A.cpp
@@ -0,0 +1,71 @@
+// #include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = not true;
+#define mango(x)
+#define fastmango ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#ifdef notwatermango
+#undef mango
+#undef notwatermango
+#define mango(x) { ++water; auto x_=x; --water; cerr<<string(water, '\t')<<"\033[38;5;206m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<'\n'; }
+#endif
+#define notwatermango void solv();
+i64 powllm(i64 a, i64 b, i64 m) { a %= m; i64 res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; }
+i64 powll(i64 a, i64 b) { i64 res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& water,  const Cont& v){ water<<"["; for(auto& x:v) water<<x<<", "; return water<<"]"; }
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& water,  const pair<Ts...>& p){ return water<<"{"<<p.first<<", "<<p.second<<"}"; }
+
+#define MOD 1000000007
+#define LLINF 4202025013666420666
+#define INF 1000177013
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+// #define int i64
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return water;
+}
+
+void solv(){
+  string s;
+  cin >> s;
+  int a,b,c;
+  a=b=c=0;
+  for (int i = 0; i < sz(s); i++)
+  {
+    if(s[i] == 'A') a++;
+    if(s[i] == 'B') b++;
+    if(s[i] == 'C') c++;
+    
+  }
+  b -= a;
+  a = 0;
+  b -= c;
+  c = 0;
+  if(b){
+    cout << "NO\n";
+  } else cout <<"YES\n";
+  
+}
