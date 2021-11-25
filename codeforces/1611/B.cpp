8c7ccb8cc0caeb9794358c2534ef6a493bd02441
notwatermango <notwatermango@gmail.com>
2021-11-25 21:45:00 +0700

    Add solution for problem `B - Team Composition: Programmers and Mathematicians` Link: https://codeforces.com/contest/1611/problem/B Tags: binary search, constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1611/submission/136852240

---
+++ codeforces/1611/B.cpp
@@ -0,0 +1,78 @@
+// #include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = not true;
+#define mango(x)
+#define fastmango ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#ifdef notwatermango
+#undef mango
+#undef notwatermango
+#define mango(x) { ++water; auto x_=x; --water; cerr<<string(water, '\t')<<"\033[38;5;206m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<'\n'; }
+#endif
+#define notwatermango void solv();
+i64 powllm(i64 a, i64 b, i64 m) { a %= m; i64 res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; }
+i64 powll(i64 a, i64 b) { i64 res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& water,  const Cont& v){ water<<"["; for(auto& x:v) water<<x<<", "; return water<<"]"; }
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& water,  const pair<Ts...>& p){ return water<<"{"<<p.first<<", "<<p.second<<"}"; }
+
+#define MOD 1000000007
+#define LLINF 4202025013666420666
+#define INF 1000177013
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define int i64
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return water;
+}
+
+void solv(){
+  int m,p;
+  cin >> m >> p;
+  int ct = 0;
+  if(m > p){
+    int qq = m-p;
+    qq /= 2;
+
+    int ok = min(qq, min(p, m/3));
+    p-=ok;
+    m-=ok*3;
+    ct += ok;
+  } else if(m < p){
+    int qq = p -m;
+    qq/=2;
+    int ok = min(qq, min(m, p/3));
+    p-=ok*3;
+    m-=ok;
+    ct+=ok;
+  }
+  int res = min(m/2, p/2);
+  m-= res*2;
+  p-= res*2;
+  ct+=res;
+  // mango(ct);
+  // cout << m << ' ' << p;
+  cout << ct <<'\n';
+}
