e8581868c653531342249523e4d26e10c4ccf312
notwatermango <notwatermango@gmail.com>
2021-11-30 21:30:00 +0700

    Add solution for problem `B - A and B` Link: https://codeforces.com/contest/1278/problem/B Tags: greedy, math, *1500 Ref: https://codeforces.com/contest/1278/submission/137525625

---
+++ codeforces/1278/B.cpp
@@ -0,0 +1,82 @@
+// #include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = not true;
+#define mango(x)
+#define fastmango ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#ifdef notwatermango
+#undef mango
+#undef notwatermango
+#define mango(x) { ++water; auto x_=x; --water; cerr<<string(water, '\t')<<"\033[38;5;206m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<'\n'; }
+#endif
+#define notwatermango void solv();
+i64 powllm(i64 a, i64 b, i64 m) { a %= m; i64 res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; }
+i64 powll(i64 a, i64 b) { i64 res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& water,  const Cont& v){ water<<"["; for(auto& x:v) water<<x<<", "; return water<<"]"; }
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& water,  const pair<Ts...>& p){ return water<<"{"<<p.first<<", "<<p.second<<"}"; }
+
+#define MOD 1000000007
+#define LLINF 4202025013666420666
+#define INF 1000177013
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define int i64
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return water;
+}
+
+int dif;
+
+bool ok(int n){
+  return n*n + n < 2 * dif;
+}
+
+int f(int x){
+  return (x*x + x)/2;
+}
+
+void solv(){
+  int a,b;
+  cin >> a >> b;
+  dif = abs(a-b);
+  if(dif == 0){
+    cout << "0\n"; return;
+  }
+  int res = 0;
+  for(int i = INF; i > 0; i /= 2){
+    while(ok(res+ i)) res +=i;
+  }
+  res++;
+  int sn = f(res);
+  if(sn == dif){
+    cout << res << '\n';
+    return;
+  }
+  while(f(res) % 2 != dif%2) res ++;
+  cout << res <<'\n';
+
+
+}
