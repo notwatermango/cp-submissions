a022ecc0b272b61ca2699bfbfd6f3db056336baa
notwatermango <notwatermango@gmail.com>
2021-12-02 13:09:00 +0700

    Add solution for problem `C - Divisibility by Eight` Link: https://codeforces.com/contest/550/problem/C Tags: brute force, dp, math, *1500 Ref: https://codeforces.com/contest/550/submission/137749542

--- codeforces/550/C.cpp
+++ codeforces/550/C.cpp
@@ -23,7 +23,7 @@ i32 water = not true;
 i64 powllm(i64 a, i64 b, i64 m) { a %= m; i64 res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; }
 i64 powll(i64 a, i64 b) { i64 res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
 template<typename Ostream, typename Cont>
-typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& water,  const Cont& dp){ water<<"["; for(auto& x:dp) water<<x<<", "; return water<<"]"; }
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& water,  const Cont& v){ water<<"["; for(auto& x:v) water<<x<<", "; return water<<"]"; }
 template<typename Ostream, typename ...Ts>
 Ostream& operator<<(Ostream& water,  const pair<Ts...>& p){ return water<<"{"<<p.first<<", "<<p.second<<"}"; }
 
@@ -51,55 +51,35 @@ signed main(){
 void solv(){
   string s;
   cin >> s;
-  int n = sz(s);
-  vector<vector<int>> dp(n, vector<int>(8)), bt(n, vector<int>(8,-1));
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i < sz(s); i++)
   {
-    int si = s[i] - '0';
-    dp[i][si%8]++;
-    if(i == 0) continue;
-    for (int j = 0; j < 8; j++)
-    {
-      if(dp[i-1][j]){
-        dp[i][j]++;
-        bt[i][j] = j;
-
-        dp[i][(j*10+si) % 8]++;
-        bt[i][(j*10+si) % 8] = j;
-      }
-    }
-  }
-  if(dp[n-1][0]){
-    cout << "YES\n";
-  } else {
-    cout << "NO\n";
-    return;
-  }
-
-  for (int i = 0; i < n; i++)
-  {
-    if(dp[i][0]){
-      string res = "";
-      int ii, jj;
-      ii = i, jj = 0;
-      while (1)
-      {
-        if(bt[ii][jj] == -1){
-          res+=s[ii];
-          break;
-        }
-        if(bt[ii][jj] != jj){
-          res+=s[ii];
-        }
-        jj = bt[ii][jj];
-        ii--;
-      }
-      reverse(all(res));
-      cout << res << '\n';
-
+    if(s[i] == '0' || s[i] == '8'){
+      cout << "YES\n" << s[i] << '\n';
       return;
     }
   }
+  for (int i = 8; i < 1000; i+=8)
+  {
+    bool ok = false;
+    string st = to_string(i);
+    int pt = 0;
+    for (int j = 0; j < sz(s); j++)
+    {
+      if(s[j] == st[pt]){
+        pt ++;
+      }
+      if(pt == sz(st)){
+        ok = true;
+        break;
+      }
+    }
+    if(ok){
+      cout << "YES\n";
+      cout << i << '\n';
+      return;
+    }
+    
+  }
+  cout << "NO\n";  
   
-  mango(dp);
 }
