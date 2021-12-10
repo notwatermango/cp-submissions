fde8fd301b6df2c3c10e9d72ebe9d1095a76b522
notwatermango <notwatermango@gmail.com>
2021-12-10 18:08:00 +0700

    Add solution for problem `A - LCM Challenge` Link: https://codeforces.com/contest/235/problem/A Tags: number theory, *1600 Ref: https://codeforces.com/contest/235/submission/138622135

---
+++ codeforces/235/A.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = !not!iscntrl(isupper(isgraph(iscntrl(ispunct(isalpha(EOF))))));
+#define mango(x)
+#define f astmango cin.tie(nullptr)->sync_with_stdio(water)
+#ifdef notwatermango
+#undef mango
+#undef notwatermango
+#define mango(x) { water = 999; auto x_ = x; water = not water; cerr<<ends<<"\033[38;5;206m"<<__LINE__<<"\t: "<<#x<<" = "<<x_<<"\e[39m"<<'\n'; }
+#endif
+#define notwatermango void solv();
+i64 powllm(i64 a, i64 b, i64 m) { a %= m; i64 res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; }
+i64 powll(i64 a, i64 b) { i64 res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
+template<typename Water, typename ...Typename>
+Water& operator<<(Water& water, const pair<Typename...>& p){ return water<<'{'<<get<0>(p)<<", "<<get<1>(p)<<'}'; }
+template<typename Water, typename Fire>
+Water& operator<<(Water& water, const Fire& v){ water<<'['; for(auto i: v) water<<i<<", "; return water<<']'; }
+template<typename _const>
+vector<_const> tov(_const x[], i32 xs) { return vector<_const> (x, x + xs); }
+
+#define INF 1000177013
+#define LLINF 4202025013666420666
+#define dbg(x) tov(x, sizeof x / sizeof x[0]);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define MOD 1000000007
+#define mp make_pair
+#define int i64
+
+notwatermango
+
+signed main(){
+  solv();
+}
+
+void solv(){
+  int n; cin >> n;
+  if(n == 1){
+    cout << "1\n";
+  } else if(n == 2){
+    cout << "2\n";
+  } else {
+    if(n%2){
+      cout << lcm(n, lcm(n-1,n-2))<<'\n';
+    } else{
+      if(n%3 == 0){
+        cout << lcm(n-1,lcm(n-2,n-3)) <<'\n';
+      } else
+      cout << lcm(n, lcm(n-1, n-3)) <<'\n';
+    }
+  }
+  
+  
+}
