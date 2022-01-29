ba52132c8258136766e8bfbebc8bd2ceec363ae9
notwatermango <notwatermango@gmail.com>
2022-01-29 19:07:00 +0700

    Add solution for problem `A - Bridge and Sheets` Link: https://atcoder.jp/contests/arc134/tasks/arc134_a Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/arc134/submissions/28859388

---
+++ atcoder/arc134/A.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = !not!iscntrl(isupper(isgraph(iscntrl(ispunct(isalpha(EOF))))));
+#define mango(x)
+#define fastmango cin.tie(nullptr)->sync_with_stdio(water)
+#ifdef notwatermango
+#undef mango
+#undef notwatermango
+#define mango(x) { water = 999; auto x_ = x; water = not water; cerr<<ends<<"\033[38;5;206m"<<__LINE__<<"\t: "<<#x<<" = "<<x_<<"\e[39m"<<'\n'; }
+#endif
+#define notwatermango void solv();
+i64 powllm(i64 a, i64 b, i64 m) { a %= m; i64 res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; }
+i64 powll(i64 a, i64 b) { i64 res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
+template<typename Water, typename ...Typename>
+Water& operator<<(Water&water,pair<Typename...>&p){return water<<'{'<<get<0>(p)<<", "<<get<1>(p)<<'}';}
+template<typename Water, typename Fire>
+Water& operator<<(Water&water,vector<vector<Fire>>v){water<<"\n{\n";for(auto i:v){water<<"\t[ ";for(auto j: i)water\
+// <<left<<setw(8) \M
+<<j<<' ';water<<"]\n";};return water<<"}";}
+template<typename Water, typename Fire>
+Water& operator<<(Water&water,Fire&v){water<<'[';for(auto i: v)water<<i<<", ";return water<<']';}
+template<typename Mango>
+vector<Mango> tov(Mango x[], i32 xs) { return vector<Mango> (x, x + xs); }
+
+#define INF 1000177013
+#define LLINF 4202025013666420666
+#define dbg(x) tov(x, sizeof x / sizeof x[0]);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define int i64
+#define MOD                                          1000000007
+#define YSS                                          112345
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  // cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return water;
+}
+
+void solv(){
+  int n; cin >> n;
+  int l,w; cin >> l >> w;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+  }
+  int now = w;
+  int tarps = 1;
+  if(a[0] == 0) {
+    now = 0;
+    tarps = 0;
+  }
+  for (int i = 0; i < n; i++)
+  {
+    if(now < a[i]) {
+      mango(now);
+      tarps += (a[i]-now)/w;
+      if((a[i]-now) % w != 0) tarps++;
+      now = a[i]+w;
+      mango(now);
+    } else {
+      now = a[i]+w;
+    }
+  }
+  if(now < l) {
+    tarps += (l-now)/w;
+    if((l-now) % w ) tarps++;
+  }
+  cout << tarps << '\n';
+  
+  
+
+}
