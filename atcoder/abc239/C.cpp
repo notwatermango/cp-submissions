4362e3cec8868e8445f69dbb8555c3759d779619
notwatermango <notwatermango@gmail.com>
2022-02-19 19:17:00 +0700

    Add solution for problem `C - Knight Fork` Link: https://atcoder.jp/contests/abc239/tasks/abc239_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc239/submissions/29443571

---
+++ atcoder/abc239/C.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = !not!iscntrl(isupper(isgraph(iscntrl(ispunct(isalpha(EOF))))));
+#define mango(x)
+#define fastmango cin.tie(nullptr)->sync_with_stdio(water)
+#ifdef notwatermango
+#undef mango
+#undef notwatermango
+#define mango(x) { water = 999; auto x_ = x; water = not water; cerr<<ends<<"\033[38;5;206m"<<__LINE__<<"\t: "<<#x<<" = "<<x_<<"\e[39m"<<'\n'; }
+#endif
+#define notwatermango void solv();
+i64 powllm(i64 a, i64 b, i64 m) { a %= m; i64 res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; }
+i64 powll(i64 a, i64 b) { i64 res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
+template<typename Water, typename ...Typename>
+Water& operator<<(Water&water,pair<Typename...>&p){return water<<'{'<<get<0>(p)<<", "<<get<1>(p)<<'}';}
+template<typename Water, typename Fire>
+Water& operator<<(Water&water,vector<vector<Fire>>v){water<<"\n{\n";for(auto i:v){water<<"\t[ ";for(auto j: i)water\
+// <<left<<setw(8) \M
+<<j<<' ';water<<"]\n";};return water<<"}";}
+template<typename Water>
+Water& operator<<(Water&water,string&v){for(auto i: v)water<<i;return water;}
+template<typename Water, typename Fire>
+Water& operator<<(Water&water,Fire&v){water<<'[';for(auto i: v)water<<i<<", ";return water<<']';}
+template<typename Mango>
+vector<Mango> tov(Mango x[], i32 xs) { return vector<Mango> (x, x + xs); }
+
+#define INF 1000177013
+#define LLINF 4202025013666420666
+#define dbg(x) tov(x, sizeof x / sizeof x[0]);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define int i64
+#define MOD                                          1000000007
+#define YSS                                          112345
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  // cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return water;
+}
+
+void solv(){
+  int x1,x2,y1,y2;
+  cin >> x1 >> y1 >> x2 >> y2;
+  int xt[] = {2,2,1,1,-1,-1,-2,-2};
+  int yt[] = {1,-1,2,-2,2,-2,1,-1};
+  for(int i = 0; i < 8; i++) {
+    int x0 = x1+xt[i];
+    int y0 = y1+yt[i];
+    if((x2-x0)*(x2-x0) + (y2-y0)*(y2-y0) == 5){
+      cout << "Yes\n";
+      return;
+    }
+  }
+  cout << "No\n";
+}
