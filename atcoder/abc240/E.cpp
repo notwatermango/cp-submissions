29fa709ab230de21900934ff74a256794d830eaa
notwatermango <notwatermango@gmail.com>
2022-02-24 21:55:00 +0700

    Add solution for problem `E - Ranges on Tree` Link: https://atcoder.jp/contests/abc240/tasks/abc240_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc240/submissions/29626714

---
+++ atcoder/abc240/E.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = !not!iscntrl(isupper(isgraph(iscntrl(ispunct(isalpha(EOF))))));
+#define mango(x)
+#define fastmango cin.tie(nullptr)->sync_with_stdio(water)
+#ifdef notwatermango
+#undef mango
+#undef notwatermango
+#define mango(x) { water = 999; auto x_ = x; water = not water; cerr<<ends<<"\033[38;5;206m"<<__LINE__<<"\t: "<<#x<<" = "<<x_<<"\e[39m"<<'\n'; }
+#endif
+#define notwatermango void solv();
+i64 powllm(i64 a, i64 b, i64 m) { a %= m; i64 res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; }
+i64 powll(i64 a, i64 b) { i64 res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
+template<typename Water, typename ...Typename>
+Water& operator<<(Water&water,pair<Typename...>&p){return water<<'{'<<get<0>(p)<<", "<<get<1>(p)<<'}';}
+template<typename Water, typename Fire>
+Water& operator<<(Water&water,vector<vector<Fire>>v){water<<"\n{\n";for(auto i:v){water<<"\t[ ";for(auto j: i)water\
+// <<left<<setw(8) \M
+<<j<<' ';water<<"]\n";};return water<<"}";}
+template<typename Water>
+Water& operator<<(Water&water,string&v){for(auto i: v)water<<i;return water;}
+template<typename Water, typename Fire>
+Water& operator<<(Water&water,Fire&v){water<<'[';for(auto i: v)water<<i<<", ";return water<<']';}
+template<typename Mango>
+vector<Mango> tov(Mango x[], i32 xs) { return vector<Mango> (x, x + xs); }
+
+#define INF 1000177013
+#define LLINF 4202025013666420666
+#define dbg(x) tov(x, sizeof x / sizeof x[0]);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define int i64
+#define MOD                                          1000000007
+#define YSS                                          112345
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  // cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return water;
+}
+
+vector<vector<int>> akachan;
+vector<vector<int>> adj;
+vector<int> lvl;
+vector<bool> vis;
+vector<pair<i32,i32>> res;
+int ct = 0;
+
+pair<i32,i32> dfs(int now) {
+
+  if(akachan[now].empty()) {
+    ct++;
+    res[now] = mp(ct,ct);
+    return res[now];
+  }
+
+  int mn, mx;
+  mn = LLINF;
+  mx = -mn;
+  for(auto e: akachan[now]) {
+    auto ret = dfs(e);
+    mn = min(mn, (int) ret.first);
+    mx = max(mx, (int) ret.second);
+  }
+  res[now].first = mn;
+  res[now].second = mx;
+  return res[now];
+
+}
+
+void solv(){
+  int n; cin >> n;
+  akachan.assign(n, vector<int>());
+  res.assign(n, pair<i32,i32>());
+  adj.assign(n, vector<int>());
+  lvl.assign(n, 0);
+  vis.assign(n, 0);
+  for (int i = 0; i < n-1; i++)
+  {
+    int fr, to;
+    cin >> fr >> to;
+    fr--;
+    to--;
+    adj[fr].push_back(to);
+    adj[to].push_back(fr);
+  }
+
+  {
+    queue<i32> q;
+    q.push(0);
+    vis[0] = 1;
+    while(not q.empty()) {
+      int now = q.front();
+      q.pop();
+
+      for(auto e: adj[now]) {
+        if(not vis[e]) {
+          akachan[now].push_back(e);
+          q.push(e);
+          vis[e] = 1;
+        }
+      }
+    }
+  }
+
+  mango(akachan[0]);
+
+  {
+    dfs(0);
+  }
+  
+  for(auto e:res) {
+    int a = e.first;
+    int b = e.second;
+    cout << a << ' ' << b << '\n';
+  }
+
+
+
+  
+}
