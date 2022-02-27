989e66c5c6d8853cce97c914d5d1f8c098d6a989
notwatermango <notwatermango@gmail.com>
2022-02-27 17:59:00 +0700

    Add solution for problem `E - Putting Candies` Link: https://atcoder.jp/contests/abc241/tasks/abc241_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc241/submissions/29738846

---
+++ atcoder/abc241/E.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = !not!iscntrl(isupper(isgraph(iscntrl(ispunct(isalpha(EOF))))));
+#define mango(x)
+#define fastmango cin.tie(nullptr)->sync_with_stdio(water)
+#define precisemango(x) cout << fixed << setprecision(x)
+#ifdef notwatermango
+#undef mango
+#undef notwatermango
+#define mango(x) { water = 999; auto x_ = x; water = not water; cerr<<ends<<"\033[38;5;206m"<<__LINE__<<"\t: "<<#x<<" = "<<x_<<"\e[39m"<<'\n'; }
+#endif
+#define notwatermango void solv();
+i64 powllm(i64 a, i64 b, i64 m) { a %= m; i64 res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; }
+i64 powll(i64 a, i64 b) { i64 res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
+template<typename Water, typename ...Typename>
+Water& operator<<(Water&water,pair<Typename...>&p){return water<<'{'<<get<0>(p)<<", "<<get<1>(p)<<'}';}
+template<typename Water, typename Fire>
+Water& operator<<(Water&water,vector<vector<Fire>>v){water<<"\n{\n";for(auto i:v){water<<"\t[ ";for(auto j: i)water\
+// <<left<<setw(8) \M
+<<j<<' ';water<<"]\n";};return water<<"}";}
+template<typename Water>
+Water& operator<<(Water&water,string&v){for(auto i: v)water<<i;return water;}
+template<typename Water, typename Fire>
+Water& operator<<(Water&water,Fire&v){water<<'[';for(auto i: v)water<<i<<", ";return water<<']';}
+template<typename Mango>
+vector<Mango> tov(Mango x[], i32 xs) { return vector<Mango> (x, x + xs); }
+
+#define INF 1000177013
+#define LLINF 4202025013666420666
+#define dbg(x) tov(x, sizeof x / sizeof x[0]);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define int i64
+#define MOD                                          1000000007
+#define YSS                                          112345
+
+notwatermango
+
+signed main(){
+  fastmango;
+  i32 tc = 1;
+  // cin >> tc;
+  for(i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return water;
+}
+
+void solv(){
+  int n; cin >> n;
+  int k; cin >> k;
+
+  int arr[n];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+  
+
+  map<int,int> ct;
+
+  int now = 0;
+  map<int,int> ts;
+
+  int star, en, len;
+  star = en = len = -1;
+
+  int qt = -1;
+  for (int i = 0; i < k; i++)
+  {
+    if(ct.count(now%n)) {
+      star = ct[now%n];
+      en = i;
+      len = en-star;
+      qt = now - ts[star];
+      break;
+    }
+    ct[now%n] = i;
+    ts[i] = now;
+    now += arr[now%n];
+  }
+  if(qt == -1) {
+    cout << now << '\n';
+    return;
+  }
+
+  mango(mp(star, en));
+
+  mango(qt);
+  mango(now);
+
+  k -= en;
+
+  now += (k/len) * qt;
+  mango(mp(k,len));
+  mango(now);
+
+  for (int i = 0; i < k%len; i++)
+  {
+    now += arr[now%n];
+  }
+  cout << now << '\n';
+  
+
+
+  
+
+
+}
