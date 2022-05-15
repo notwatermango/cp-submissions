e31e8f5ac90883a0484cd20a6be94964daf09af4
notwatermango <notwatermango@gmail.com>
2022-05-15 18:40:00 +0700

    Add solution for problem `D1 - Two Hundred Twenty One (easy version)` Link: https://codeforces.com/contest/1562/problem/D1 Tags: data structures, dp, math, *1700 Ref: https://codeforces.com/contest/1562/submission/157300445

---
+++ codeforces/1562/D1.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = !not!iscntrl(isupper(isgraph(iscntrl(ispunct(isalpha(EOF))))));
+#define mango(x)
+#define fastmango cin.tie(nullptr)->sync_with_stdio(water)
+#define precisemango(x) cout << fixed << setprecision(x)
+#ifdef notwatermango
+#undef mango
+#undef notwatermango
+#define mango(x) { water = 999; auto x_ = x; water = not water; cerr<<ends<<"\033[38;5;206m"<<__LINE__<<"\t: "<<#x<<" = "<<x_<<"\e[39m"<<'\n'; }
+#endif
+#define notwatermango void solv()
+i64 powllm(i64 a, i64 b, i64 m) { a %= m; i64 res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; }
+i64 powll(i64 a, i64 b) { i64 res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
+template<typename Water, typename ...Typename>
+Water& operator<<(Water&water,pair<Typename...>&p){return water<<'{'<<get<0>(p)<<", "<<get<1>(p)<<'}';}
+template<typename Water, typename Fire>
+Water& operator<<(Water&water,vector<vector<Fire>>v){water<<"\n{\n";for(auto i:v){water<<"\t[ ";for(auto j: i)water\
+// <<left<<setw(8) \笑
+<<j<<' ';water<<"]\n";};return water<<"}";}
+template<typename Water>
+Water& operator<<(Water&water,string&v){for(auto i: v)water<<i;return water;}
+template<typename Water, typename Fire>
+Water& operator<<(Water&water,Fire&v){water<<'[';for(auto i: v)water<<i<<", ";return water<<']';}
+template<typename Mango>
+vector<Mango> tov(Mango x[], i32 xs) { return vector<Mango> (x, x + xs); }
+
+#define INF 1000177013
+#define LLINF 4202025013666420666
+#define dbg(x) tov(x, sizeof x / sizeof x[0]);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define int i64
+#define MOD                                          1000000007
+#define YSS                                          112345
+
+notwatermango;
+
+signed main() {
+  fastmango;
+  i32 tc = 1;
+  cin >> tc;
+  for (i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return water;
+}
+
+/* fenw tree */
+struct FenwickTree {
+  vector<int> bit; // binary indexed
+  int n;
+
+  FenwickTree(int n) {
+    this->n = n;
+    bit.assign(n, 0);
+  }
+
+  FenwickTree(vector<int> a) : FenwickTree(a.size()) {
+    for (size_t i = 0; i < a.size(); i++)
+      add(i, a[i]);
+  }
+
+  int sum(int r) {
+    int ret = 0;
+    for (; r >= 0; r = (r & (r + 1)) - 1)
+      ret += bit[r];
+    return ret;
+  }
+
+  int sum(int l, int r) { return sum(r) - sum(l - 1); }
+
+  void add(int idx, int delta) {
+    for (; idx < n; idx = idx | (idx + 1))
+      bit[idx] += delta;
+  }
+};
+/* ~fenw tree */
+
+void solv() {
+  int n; cin >> n;
+  int q; cin >> q;
+  FenwickTree fa(n);
+  for (int i = 0; i < n; ++i) {
+    char cj; cin >> cj;
+    int ww = (cj == '+'? 1:-1);
+    if (i % 2) ww *= -1;
+    fa.add(i, ww);
+  }
+  for (int i = 0; i < q; ++i) {
+    int l, r; cin >> l >> r; l--, r--;
+    int sw = fa.sum(l, r);
+    if (sw % 2) cout << "1\n";
+    else if (sw) cout << "2\n";
+    else cout << "0\n";
+  }
+}
