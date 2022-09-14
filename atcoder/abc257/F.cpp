75da0735123141fe03ae1bfa7cc196d34cbb90c7
notwatermango <notwatermango@gmail.com>
2022-09-14 11:19:00 +0700

    Add solution for problem `F - Teleporter Setting` Link: https://atcoder.jp/contests/abc257/tasks/abc257_f Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc257/submissions/34847798

---
+++ atcoder/abc257/F.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t i64;
+typedef int32_t i32;
+
+i32 water = !not!iscntrl(isupper(isgraph(iscntrl(ispunct(isalpha(EOF))))));
+#define mango(x)
+#define fastmango cin.tie(nullptr)->sync_with_stdio(water)
+#define precisemango(x) cout << fixed << setprecision(x)
+#ifdef notwatermango
+#undef mango
+#undef notwatermango
+#define mango(x) { water = 999; auto x_ = x; water = not water; cerr<<ends<<"\033[38;5;206m"<<__LINE__<<"\t: "<<#x<<" = "<<x_<<"\e[39m"<<'\n'; }
+#endif
+#define notwatermango void solv()
+i64 powllm(i64 a, i64 b, i64 m) { a %= m; i64 res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; }
+i64 powll(i64 a, i64 b) { i64 res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
+template<typename Water, typename ...Typename>
+Water& operator<<(Water&water,pair<Typename...>&p){return water<<'{'<<get<0>(p)<<", "<<get<1>(p)<<'}';}
+template<typename Water, typename Fire>
+Water& operator<<(Water&water,vector<vector<Fire>>v){water<<"\n{\n";for(auto i:v){water<<"\t[ ";for(auto j: i)water\
+// <<left<<setw(8) \笑
+<<j<<' ';water<<"]\n";};return water<<"}";}
+template<typename Water>
+Water& operator<<(Water&water,string&v){for(auto i: v)water<<i;return water;}
+template<typename Water, typename Fire>
+Water& operator<<(Water&water,Fire&v){water<<'[';for(auto i: v)water<<i<<", ";return water<<']';}
+template<typename Mango>
+vector<Mango> tov(Mango x[], i32 xs) { return vector<Mango> (x, x + xs); }
+
+#define INF 1000177013
+#define LLINF 4202025013666420666
+#define dbg(x) tov(x, sizeof x / sizeof x[0]);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define int i64
+#define MOD                                          1000000007
+#define YSS                                          112345
+
+notwatermango;
+
+signed main() {
+  fastmango;
+  i32 tc = 1;
+  // cin >> tc;
+  for (i32 i = 0; i < tc; ++i) {
+    // cout << "Case #" << i+1 << ": ";
+    solv();
+  }
+  return water;
+}
+
+void solv() {
+  int n; cin >> n;
+  int m; cin >> m;
+  vector<int> vis(n);
+  vector<vector<int>> adj(n+1);
+  for (int i = 0; i < m; ++i) {
+    int fr, to; cin >> fr >> to;
+    adj[fr].push_back(to);
+    adj[to].push_back(fr);
+  }
+  auto dj = [&](int sv) {
+    vector<int> dist(n+1, INF);
+    queue<int> q;
+    q.push(sv); dist[sv] = 0;
+    while(q.size()) {
+      int v = q.front(); q.pop();
+      for (auto u: adj[v]) {
+        if (dist[u] != INF) continue;
+        dist[u] = dist[v] + 1;
+        q.push(u);
+      }
+    }
+    return dist;
+  };
+  vector<int> d1 = dj(1), dn = dj(n);
+  for (int i = 1; i <= n; ++i) {
+    int res = d1[n];
+    mango(res);
+    res = min(res, d1[i] + dn[0]);
+    mango(res);
+    res = min(res, d1[0] + dn[i]);
+    if (res == INF) res = -1;
+    cout << res << ' ';
+  }
+}
